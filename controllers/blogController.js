6670d5807d1fc2b9aa6701ce40df4b11afff46bf
Himanshu <kyadav.9643@gmail.com>
2023-10-21 17:26:46 +0530

    added new route
    added getbyid and deleteby id for blogs

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -26,7 +26,7 @@ export const addBlog =async (req,res,next) =>{
 export const updateBlog = async (req,res,next) => {
     try {
         const {title, description} = req.body;
-        const blogId = req.param.id;
+        const blogId = req.params.id;
 
         const blog = await Blog.findByIdAndUpdate(blogId,{
             title,
@@ -36,9 +36,38 @@ export const updateBlog = async (req,res,next) => {
         if(!blog){
             return res.status(500).json({message: "Unable to update the blog"});
         }
+
         return res.status(200).json({blog});
 
     } catch (error) {
-     console.log(error)   
+        console.log(error)   
+    }
+}
+
+export const getById = async (req, res, next) =>{
+    try {
+        const {id} = req.params;
+        const blog = await Blog.findById(id);
+
+        if(!blog) 
+            return res.status(404).json({message: "Unable to find blog"});
+
+        return res.status(200).json({blog});
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const deleteBlog = async (req, res, next) =>{
+    try {
+        const {id} = req.params;
+        const blog = await Blog.findByIdAndRemove(id);
+
+        if(!blog)
+            return res.status(404).json({message:"No blog found"});
+
+        return res.status(200).json({message: "Successfully deleted"})
+    } catch (error) {
+        console.log(error);
     }
 }
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,10 +1,11 @@
 import  Express  from "express";
-import { addBlog, getAllBlogs, updateBlog } from "../controllers/blogController.js";
+import { addBlog, deleteBlog, getAllBlogs, getById, updateBlog } from "../controllers/blogController.js";
 
 const router = Express.Router();
 
 router.get("/",getAllBlogs);
 router.post("/add", addBlog);
-router.put("/update/:id", updateBlog)
-
+router.put("/update/:id", updateBlog);
+router.get("/:id",getById);
+router.delete("/:id", deleteBlog);
 export default router;
